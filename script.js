9ad7beea17567cb56d0b02e49b86bea51b6bec8c
Brandon Noel <>
2022-09-27 02:22:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var rock = 1;
+var paper = 2;
+var sicssors = 3;
+
+document.write(paper + "<br>");
+document.write("----------------" + "<br>");
+document.write(paper);
+
+function test(){
+  document.write(paper + 3);
+}
