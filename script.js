e1b44e52e63d0c7da84051227638ccfd4724aaba
Brandon Noel <brandon_noel@live.ca>
2022-10-07 04:23:54 +0000

    small changes

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var rock = 1;
 var paper = 2;
 var sicssors = 3;
 
-document.getElementById("game").innerHTML= "ye";
 document.write(paper + "<br>");
 document.write("----------------" + "<br>");
 test1 = document.write(paper);
 
 function test(){
+  document.getElementById("game").innerHTML = paper + 1;
 }
