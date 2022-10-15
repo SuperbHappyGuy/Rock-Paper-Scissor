9fec3ec17e9e2915c8d91b64357e4424a70cc7c7
Brandon Noel <brandon_noel@live.ca>
2022-10-15 01:27:08 +0000

    Renamed files and added function to remove buttons on click

--- Js/index.js
+++ Js/index.js
@@ -6,6 +6,18 @@ document.write(paper + "<br>");
 document.write("----------------" + "<br>");
 test1 = document.write(paper);
 
+
+window.onload = function() {
+  const btns = document.querySelectorAll('.btn');
+
+  btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      btn.style.display = "none";
+    });
+  });
+}
+
 function test(){
   document.getElementById("game").innerHTML = paper + 1;
 }
+
